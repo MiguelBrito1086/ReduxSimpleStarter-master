2ed0ebecc6aac19bd49a1ecf45be27c382658fce
MiguelBrito1086 <mbrito1086@gmail.com>
2017-11-06 13:48:11 -0500

    m

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
 
 const API_KEY = 'AIzaSyCrLqcqep0D6lfTYwSkvA6uDcPrz6WnzpY';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-)};
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
