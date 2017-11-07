7c2d600b5eded28ef79e3fbfdb732f0e3cf9e003
MiguelBrito1086 <mbrito1086@gmail.com>
2017-11-07 11:13:55 -0500

    added video and select function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
-
+import VideoList from './components/videolist';
+import VideoDetail from './components/videodetail';
 const API_KEY = 'AIzaSyCrLqcqep0D6lfTYwSkvA6uDcPrz6WnzpY';
 
 class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
@@ -20,6 +27,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos } />
       </div>
     );
   }
